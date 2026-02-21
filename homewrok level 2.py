d360fcf6be768cc3436e66ea7c372060af0fbed2
manianikoloz14 <mania.nikoloz14@gmail.com>
2026-02-21 18:50:17 +0400

    Add files via upload

---
+++ homewrok level 2.py
@@ -0,0 +1,86 @@
+# რა არის string? string არის ტექსტი რომელიც გამოიყენება სიტყვის,წინადადების ან ზოგადად ტექსტის შესანახად. სტრინგი ბრჭყალებში იწერება.
+
+# რა არის integer? integer არის მთელი რიცხვი არა ათწილადი.
+
+# რა არის float? float არის ათწილადი რიცხვი. იგი აუცილებლად წერტილს უნდა შეიცავდეს
+
+# რა არის ცვლადი? ცვლადი არის ადგილი სდაც ვინახავთ რახაც იმფორმაციას
+# მაგალითად შეიგვიძლია რიცხვი,სახელი ასაკი რიცხვი და ა. შ.
+
+
+
+#string,integer,floar-ის გამოყენება:)
+
+
+#string
+print('hello')               #1
+print('my name is')          #2
+print('nikoloz mania')       #3
+print('i am from georgia')   #4
+print('my age is')           #5
+#integer
+print(14)     #1
+print(69)     #2
+print(67)     #3
+print(21)     #4
+print(1234)   #5
+#float
+print(1.4)     #1
+print(6.7)     #2
+print(6.9)     #3
+print(2.1)     #4
+print(12.34)   #5
+
+
+#ცვლადების შექმნდა და გამოყენება:)
+
+name = 'nikoloz mania'
+code = 'coding'
+love = 'loves'
+reason = 'cuz it is'
+work = 'ABSOLUTE CHAD WORK'
+
+print( name + " " + code + " " + love + " " +  reason + " " +  work)
+#ან
+print( name, code, love, reason, work)
+
+# მიმატება
+print(2 + 3)
+print(5 + 7)
+print(10 + 4)
+
+# გამოკლება
+print(10 - 3)
+print(8 - 2)
+print(20 - 5)
+
+# გამრავლება
+print(3 * 4)
+print(5 * 6)
+print(2 * 9)
+
+# გაყოფა
+print(10 / 2)
+print(9 / 3)
+print(8 / 4)
+
+
+
+
+
+
+
+
+
+
+# სამი ცვლადის ჯამი
+
+num1 = 6
+num2 = 7
+num3 = 67
+
+print(num1 + num2 + num3)
+print(num1 / num2 / num3)
+print(num1 * num2 * num3)
+
+
